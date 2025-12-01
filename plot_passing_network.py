736b40b7604e3d229da5cc350f81686806e1356e
Christoph Achermann <christoph.achermann@gmail.com>
2025-12-01 01:43:15 +0100

    Passpfeile gezeichnet

--- plot_passing_network.py
+++ plot_passing_network.py
@@ -51,6 +51,7 @@ def get_passnetwork_before_sub (indata):
         df_pass_network["first_sub_minute"] = earliest_sub
         df_pass_network["dx"] = df_pass_network["x_recipient"] - df_pass_network["x_player"]
         df_pass_network["dy"] = df_pass_network["y_recipient"] - df_pass_network["y_player"]
+        df_pass_network["team_name"] = df_current_team["team_name"].unique()[0]
         team_list.append(df_pass_network) 
     
     outdata = pd.concat([team_list[0], team_list[1]], ignore_index=True)
@@ -61,6 +62,7 @@ df_game_pass_network = get_passnetwork_before_sub(df)
 
 for team_id in df_game_pass_network["team_id"].unique():
     df_network_map_current_team = df_game_pass_network[df_game_pass_network["team_id"]==team_id]
+    current_team_name = df_network_map_current_team["team_name"].unique()[0]
     # draw player on pitch
     pitch = Pitch(line_color = "black")
     fig, ax = pitch.draw(figsize=(10, 7))
@@ -73,8 +75,45 @@ for team_id in df_game_pass_network["team_id"].unique():
         c="red",
         s=40,                          # circleSize²
         alpha=1)
+    
+    # Spielerlabels hinzufügen
+    dx = np.random.uniform(-1, 1)
+    dy = np.random.uniform(0.5, 1.5)
+    for _, row in df_player_pos.iterrows():
+        ax.text(
+            row["x_player"] + dx,
+            row["y_player"] + dy,
+            row["player_name_player"],
+            fontsize=9,
+            color="black",
+            ha="center",
+            va="center"
+        )
     #fig.suptitle(f"{team_} (red) and Opponent (blue) shots", fontsize=24)    
     #fig.set_size_inches(10, 7)
+    
+    # Jetzt die Passpfeile zeichnen
+    var_mindest_pass_zahl = 8
+    df_network_map_current_team_draw =  df_network_map_current_team[df_network_map_current_team["paesse_zeit_normiert"] >= var_mindest_pass_zahl ]
+    for _, row in df_network_map_current_team_draw.iterrows():
+
+        x0 = row["x_player"]
+        y0 = row["y_player"]
+        dx = row["dx"]
+        dy = row["dy"]
+        width = row["paesse_zeit_normiert"] * 0.2   # Faktor anpassen für Dicke
+
+        ax.arrow(
+            x0, y0,
+            dx, dy,
+            head_width=1.5,        # Größe der Pfeilspitze
+            length_includes_head=True,
+            color="red",
+            alpha=0.7,
+            linewidth=0,
+            width=.1             # Linienbreite = Pass-Stärke
+        )
+    plt.savefig("pass_network_" + current_team_name + ".png")
     plt.show()
 
 
